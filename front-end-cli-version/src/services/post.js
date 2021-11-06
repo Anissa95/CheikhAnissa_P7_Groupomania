3a453a2ba464688aaaac09ecb25e5b2a356e0b95
Anissa95 <f.anissa@hotmail.fr>
2021-11-06 07:57:31 +0100

    Etablir la communication entre backend et frontend grace à l'URL et via la bibliothèque AXIOS (service)

---
+++ front-end-cli-version/src/services/post.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+// Etablir la communication entre backend et frontend grace à l'URL et via la bibliothèque AXIOS
+export const getPosts = (post) => axios.get("http://localhost:3000/post", post);
+export const getOnePost = (post) => axios.get("http://localhost:3000/post/:id", post);
+export const createPost = (post) =>
+    axios.post("http://localhost:3000/post", post);
+export const updatePost = (post) =>
+    axios.put("http://localhost:3000/post/:id", post);
+export const deletePost = (post) =>
+    axios.delete("http://localhost:3000/post/:id", post);
