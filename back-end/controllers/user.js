4d48cc9e37a2c95f575d6696c4c134bff512ff90
Anissa95 <f.anissa@hotmail.fr>
2021-11-08 14:41:01 +0100

    Implémentation de la methode Deleteuser

--- back-end/controllers/user.js
+++ back-end/controllers/user.js
@@ -54,12 +54,69 @@ exports.login = (req, res, next) => {
 };
 //Supprission d'un utilisateur
 exports.deleteUser = (req, res, next) => {
+    // on recupère userid du token jwt
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.TOKEN_KEY);
+    const userId = decodedToken.userId;
+    console.log(req.body.password);
+    db.user
+        .findOne({
+            where: { id: userId },
+        })
+        .then((user) => {
+            if (!user) {
+                return res.status(401).json({ error: "Utilisateur non trouvé!" });
+            }
+            // comparer le MDP saisi et avec  celui la BDD
+            bcrypt
+                .compare(req.body.password, user.dataValues.password)
+                .then((valid) => {
+                    if (!valid) {
+                        return res.status(401).json({ error: "Mot de passe incorrecte !" });
+                    }
+                    //Supprimer le user avec tt ses posts 
+                    db.user.destroy({ where: { id: userId } });
+                    //  Tous les posts d'un user
+                    db.post
+                        .findAll({ where: { userId: userId } })
+                        .then((result) => {
+                            let postsId = [];
+                            let postsUrlImage = [];
+                            for (let i = 0; i < result.length; i++) {
+                                postsId.push(result[i].id);
+                                postsUrlImage.push(result[i].imgURL.split("/images/")[1]);
+                            }
+                            // Images des posts
+                            for (let i = 0; i < postsUrlImage.length; i++) {
+                                fs.unlink(`images/${postsUrlImage[i]}`, (err) => {
+                                    if (err) {
+                                        console.log(`images/${postsUrlImage[i]} not found !`);
+                                    } else {
+                                        console.log(`deleted old images/${postsUrlImage[i]}`);
+                                    }
+                                });
+                            }
+                            //Suppression des commentaires du user et commentaires des post d'un user supprimé  
+                            db.comment.destroy({
+                                where: {
+                                    [Op.or]: [{ authorId: userId }, { postId: postsId }],
+                                },
+                            });
+                            //Suppression des posts d'un user
+                            db.post.destroy({ where: { userId: userId } }).then(() => {
+                                res.status(200).json({ message: "Utilisateur supprimé" });
+                            });
+                        })
+                        .catch((error) => res.status(401).json({ error }));
+                });
+        })
+        .catch((error) => res.status(500).json({ error }));
 
 };
 //Afficher l'administrateur
 exports.getAdmin = (req, res, next) => {
     const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+    const decodedToken = jwt.verify(token, process.env.TOKEN_KEY);
     const userId = decodedToken.userId;
     db.user
         .findOne({
@@ -84,9 +141,9 @@ exports.getAdmin = (req, res, next) => {
 
 };
 //Trouver un user 
-exports.findUser = (req, res, next) => {
+exports.getUser = (req, res, next) => {
     const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
+    const decodedToken = jwt.verify(token, process.env.TOKEN_KEY);
     const userId = decodedToken.userId;
     db.user
         .findOne({
